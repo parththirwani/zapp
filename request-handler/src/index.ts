f1cb73fa4a1b02fe6d89e91420934ce2bd5e984b
parththirwani <124462644+parththirwani@users.noreply.github.com>
2025-09-23 18:26:05 +0530

    request-handler

---
+++ request-handler/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const app = express();
+
+app.get("/*", async (req, res) => {
+    const host = req.hostname;
+
+    const id = host.split(".")[0];
+    const filePath = req.path;
+
+    const contents = await s3.getObject({
+        Bucket: "vercel",
+        Key: `dist/${id}${filePath}`
+    }).promise();
+    
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    res.set("Content-Type", type);
+
+    res.send(contents.Body);
+})
+
+app.listen(3002);
